dbd26067132f0393810aa182e89c748595b78824
Emanuel Padilla <emanuehl159@gmail.com>
2023-08-25 22:34:57 -0500

    Añado más endpoints de prueba. Parte 4 del curso.

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ app.get('/', (req, res) => {
   res.send('Hola que tal, ya funciona!');
 });
 
+app.get('/home', (req, res) => {
+  res.send('Bienvenid@ a este proyecto!!');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send(
+    'Este debería ser un mensaje diferente, pero no lo es mucho en un nivel fundamental.',
+  );
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Chaqueta de cuero',
+    price: 70,
+    category: 'clothes',
+  });
+});
+
 app.listen(port, () => {
   console.log('Servidor abierto en el puerto: ' + port);
 });
