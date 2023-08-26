60a7523d82d0b6c19b3bfc0886f37dfe8e9773c7
Emanuel Padilla <emanuehl159@gmail.com>
2023-08-25 22:12:00 -0500

    Server con express funcionando. Parte 3 del curso.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Funciona');
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hola que tal, ya funciona!');
+});
+
+app.listen(port, () => {
+  console.log('Servidor abierto en el puerto: ' + port);
+});
