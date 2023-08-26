abd00619ee0d10836195cf7699f06c8f31d9b99a
Emanuel Padilla <emanuehl159@gmail.com>
2023-08-25 23:22:34 -0500

    GET con parametros. Reto completado. Parte 7 del curso.

--- index.js
+++ index.js
@@ -2,6 +2,46 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+const productsList = [
+  {
+    'product id': 1,
+    name: 'Chaqueta de cuero',
+    price: 70,
+    category: 'clothes',
+  },
+  {
+    'product id': 2,
+    name: 'Pantalones azules',
+    price: 30,
+    category: 'clothes',
+  },
+  {
+    'product id': 3,
+    name: 'Telefono Nokia antiguo',
+    price: 20,
+    category: 'electronics',
+  },
+  {
+    'product id': 4,
+    name: 'Chocolatina Jet Grande',
+    price: 99999,
+    category: 'others',
+  },
+  {
+    'product id': 5,
+    name: 'Funko de Spider-Man',
+    price: 48,
+    category: 'others',
+  },
+];
+
+const usersList = [
+  { id: 1, name: 'Juan', role: 'Admin' },
+  { id: 2, name: 'Lucia', role: 'Manager' },
+  { id: 3, name: 'Esteban', role: 'Crew' },
+  { id: 4, name: 'Emanuel', role: 'Client' },
+];
+
 app.get('/', (req, res) => {
   res.send('Hola que tal, ya funciona!');
 });
@@ -17,10 +57,42 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
+  res.json(productsList);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
-    name: 'Chaqueta de cuero',
-    price: 70,
-    category: 'clothes',
+    'product id': id,
+    name: 'Producto de prueba',
+    price: 68,
+    category: 'others',
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
+// Reto: Crear los endpoints para users, categories, orders y los que se te ocurran.
+app.get('/users', (req, res) => {
+  res.json(usersList);
+});
+
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  res.json(usersList[id - 1]);
+});
+
+app.get('/orders', (req, res) => {
+  res.json({
+    date: 'July 26 / 2023',
+    totalProducts: 4,
+    totalPrice: 999,
   });
 });
 
