877344cb51a26d78820ec0713f6ca522fefdf978
Emanuel Padilla <emanuehl159@gmail.com>
2023-09-03 00:55:14 -0500

    Integro uso de variables de entorno de forma parcial. Parte 8 del curso.

--- api/libs/postgres.pool.js
+++ api/libs/postgres.pool.js
@@ -1,11 +1,12 @@
+const config = require('../config/config');
 const { Pool } = require('pg');
 
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
 const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'root',
-  password: 'root',
-  database: 'my-ecommerce-db',
+  connectionString: URI,
 });
 
 module.exports = pool;
