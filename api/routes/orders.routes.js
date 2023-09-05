723d16566123499c43e2c698f57cf26174a8fd7f
Emanuel Padilla <emanuehl159@gmail.com>
2023-09-04 22:30:52 -0500

    Remuevo datos sin usar en orders.routes.js.

--- api/routes/orders.routes.js
+++ api/routes/orders.routes.js
@@ -2,18 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json([
-    {
-      date: 'July 26 / 2023',
-      totalProducts: 4,
-      totalPrice: 999,
-    },
-    {
-      date: 'July 27 / 2023',
-      totalProducts: 1,
-      totalPrice: 9998,
-    },
-  ]);
+  res.json();
 });
 
 module.exports = router;
