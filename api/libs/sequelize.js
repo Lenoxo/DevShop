9ff78ace7e53a567c8922b6047591ca903f91fff
Lenoxo <119215089+Lenoxo@users.noreply.github.com>
2023-09-12 22:22:13 -0500

    Migración a MySQL. Parte 12 del curso.

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -4,10 +4,10 @@ const initModels = require('../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
+  dialect: 'mysql',
   // Esta es una nueva sintaxis que se maneja en la versión más reciente de sequelize.
   logging: (query) => {
     // Se recibe el query en vez de poner true.
