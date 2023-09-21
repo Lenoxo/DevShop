257796f1df53e712f6c380b0582ee7864d3cdc0e
Lenoxo <119215089+Lenoxo@users.noreply.github.com>
2023-09-20 23:41:01 -0500

    Completo cambios para hacer deploy en Railway.

--- api/db/migrations/20230913221413-create-user-table.js
+++ api/db/migrations/20230913221413-create-user-table.js
@@ -1,14 +1,38 @@
-const { userSchema, USER_TABLE } = require('../models/user.model');
 ('use strict');
+
+const { USER_TABLE } = require('../models/user.model');
+const { DataTypes } = require('sequelize');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   // up se usa para ejecutar pasos en la migración
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable(USER_TABLE, userSchema);
+    await queryInterface.createTable(USER_TABLE, {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: DataTypes.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: DataTypes.NOW,
+      },
+    });
   },
   // down se usa para revertir los pasos de la migración
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable(USER_TABLE, userSchema);
+    await queryInterface.dropTable(USER_TABLE);
   },
 };
--- api/db/migrations/20230915035028-create-orders-table.js
+++ api/db/migrations/20230915035028-create-orders-table.js
@@ -1,11 +1,39 @@
 'use strict';
 
-const { ORDER_TABLE, orderSchema } = require('../models/order.model');
+const { ORDER_TABLE } = require('../models/order.model');
+const { DataTypes } = require('sequelize');
+const { CUSTOMER_TABLE } = require('../models/customer.model');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable(ORDER_TABLE, orderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+      },
+
+      customerId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        field: 'customer_id',
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id', // Esta debe ser siempre la llave primaria del model.
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+
+      date: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'date',
+        defaultValue: DataTypes.NOW,
+      },
+    });
   },
 
   async down(queryInterface, Sequelize) {
