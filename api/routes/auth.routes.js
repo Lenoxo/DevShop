be2a4ecd83a19b0b480d0a486eb5487e8e497e81
Lenoxo <119215089+Lenoxo@users.noreply.github.com>
2023-09-24 21:14:23 -0500

    Uso de signToken en auth.routes. Parte 10 del curso.

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
 const passport = require('passport');
+
+const secret = config.jwtSecret;
 
 router.post(
   '/login',
   passport.authenticate('local', { session: false }),
   async (req, res, next) => {
     try {
-      res.json(req.user);
+      const user = req.user;
+      const payload = {
+        sub: user.id,
+        role: user.role,
+      };
+
+      function signToken(payload, secret) {
+        return jwt.sign(payload, secret);
+      }
+
+      const token = signToken(payload, secret);
+      res.json({
+        user,
+        token,
+      });
     } catch (error) {
       next(error);
     }
